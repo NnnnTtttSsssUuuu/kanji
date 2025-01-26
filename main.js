16102543e9b6f7b4f1161668e5a5a095e0f379d7
NnnnTtttSsssUuuu <154814398+NnnnTtttSsssUuuu@users.noreply.github.com>
2025-01-26 15:37:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,148 @@
 `use strict`;
 {
-  let jsonKanji;
-  let target = 0;
+  let targetKanji = 0;
   let jtarget = 0;
   let kariItaiji = [];
   let oldQuery = "";
   let queryNo = 0;
+  let kanjiShugo = JSON.parse(localStorage.getItem('kanjiLocal'));
+  sessionStorage.setItem('checkKanji', 0); 
+
+  window.onload = function () {
+    if (!kanjiShugo) {
+      getJson();
+    }
+  }
+
+  document.querySelector('#getJson').addEventListener('click', () => {
+    getJson();
+  });
 
   //JSON読み込み
-  fetch('kanjiFile.json')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('HTTP error! status: ' + response.status);
+  function getJson() {
+    fetch('kanjiFile.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('HTTP error! status: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(kanjiFile => {
+        kanjiShugo = kanjiFile;
+        // console.log("kanjiShugo", kanjiShugo);
+        localStorage.setItem('kanjiLocal', JSON.stringify(kanjiFile));
+      })
+      .catch(error => console.error('Error loading JSON:', error));
+  }
+
+  //List.js対応
+  // const options = {
+  //   valueNames: [
+  //     'menkuten',
+  //     'unicode',
+  //     'jitai1',
+  //     'jishu',
+  //     'yomi',
+  //     'jitai2',
+  //     'jitai3',
+  //     'jitai4'
+  //   ],
+  // };
+  // const searchList = new FileList('maintableid', options);
+
+
+  //大漢字表を作成する
+  setTimeout(() => {
+    const kanjiHyo = document.querySelector('#kanjiHyo');
+    for (let i = 0; i < kanjiShugo.length; i++) {
+      if (kanjiShugo[i].junOfJitai < 2) {
+        const kanjiRow = document.createElement('tr');
+        kanjiRow.className = "kanji";
+        kanjiRow.id = kanjiShugo[i].unicode;
+
+        const kanjiMenkuten = document.createElement('td');
+        kanjiMenkuten.className = "menkuten_";
+        kanjiMenkuten.textContent = kanjiShugo[i].menkuten;
+
+        const kanjiUnicode = document.createElement('td');
+        kanjiUnicode.className = "unicode_";
+        kanjiUnicode.textContent = kanjiShugo[i].unicode;
+
+        const kanjiMidashi = document.createElement('td');
+        kanjiMidashi.className = "midashi_";
+        kanjiMidashi.textContent = kanjiShugo[i].midashi;
+
+        const kanjiMojigun = document.createElement('td');
+        kanjiMojigun.className = "jishu_";
+        kanjiMojigun.textContent = kanjiShugo[i].mojigun;
+
+        const kanjiYomi = document.createElement('td');
+        kanjiYomi.className = "yomi_";
+        kanjiYomi.textContent = kanjiShugo[i].yomi;
+
+        const kanjiJitai2 = document.createElement('td');
+        kanjiJitai2.className = "jitai2_";
+        kanjiJitai2.textContent = kanjiShugo[i].jitai2;
+
+        const kanjiJitai3 = document.createElement('td');
+        kanjiJitai3.className = "jitai3_";
+        kanjiJitai3.textContent = kanjiShugo[i].jitai3;
+
+        const kanjiJitai4 = document.createElement('td');
+        kanjiJitai4.className = "jitai4_";
+        kanjiJitai4.textContent = kanjiShugo[i].jitai4;
+
+        const kanjiRyakusetsu = document.createElement('td');
+        kanjiRyakusetsu.className = "ryakusetsu_";
+        kanjiRyakusetsu.textContent = kanjiShugo[i].ryakusetsu;
+
+        kanjiRow.appendChild(kanjiMenkuten);
+        kanjiRow.appendChild(kanjiUnicode);
+        kanjiRow.appendChild(kanjiMidashi);
+        kanjiRow.appendChild(kanjiMojigun);
+        kanjiRow.appendChild(kanjiYomi);
+        kanjiRow.appendChild(kanjiJitai2);
+        kanjiRow.appendChild(kanjiJitai3);
+        kanjiRow.appendChild(kanjiJitai4);
+        kanjiRow.appendChild(kanjiRyakusetsu);
+
+        kanjiHyo.appendChild(kanjiRow);
       }
-      return response.json();
-    })
-    .then(kanjiFile => {
-      jsonKanji = kanjiFile;
-    })
-    .catch(error => console.error('Error loading JSON:', error));
+    }
+    // console.log(kanjiHyo);
+  }, 200); //1秒間タイマー
 
 
-  //検索ボタン
+  //検索する
+  document.getElementById("searchForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const query = document.getElementById("searchInput").value.trim();
+    highlightText(query);
+    if (oldQuery === query) {
+      queryNo = queryNo + 1;
+    } else {
+      oldQuery = query;
+      queryNo = 0;
+    }
+    gotoHighlight(queryNo);
+  });
+
   function highlightText(query) {
-
     document.querySelectorAll(".highlight").forEach(el => {
       el.classList.remove("highlight");
-      console.log("ハイライト消去");
     });
-
     if (!query) return;// キーワードが空なら何もしない
-
-    console.log("検索ファンクション起動", query);
-
     const regex = new RegExp(`(${query})`, "gi"); // 検索キーワードを正規表現化
-
-    // const regex = new RegExp(`(${query})`, "g"); // 検索キーワードを正規表現化
-
-    // const regex = new RegExp(`(${query})`); // 検索キーワードを正規表現化
-
+    //body内のツリー構造を探索し、テキストだけを取り出す
     const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-
-    console.log("regex",regex);
-
     let count = 0;
     let countmatch = 0;
-
-    while (walker.nextNode()) {
-      const node = walker.currentNode;
+    let currentNode = walker.nextNode(); // 最初のノードを取得
+    // while (walker.nextNode()) {
+    while (currentNode) {
+      // const node = walker.currentNode;
+      const node = currentNode; //現在のノードを保存
+      currentNode = walker.nextNode(); //探索を続ける
       if (node.nodeValue.match(regex)) {
         const span = document.createElement("span");
         span.innerHTML = node.nodeValue.replace(regex, `<span class="highlight">$1</span>`);
@@ -56,51 +151,16 @@
       }
       count = count + 1;
     }
-    console.log("count",count,"countmatch",countmatch);
-
   }
-
-  document.getElementById("searchForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    console.log("検索イベントリスナー起動");
-
-
-    const query = document.getElementById("searchInput").value.trim();
-    highlightText(query);
-
-    if (oldQuery === query) {
-      queryNo = queryNo + 1;
-    } else {
-      oldQuery = query;
-      queryNo = 0;
-    }
-    console.log("oldQuery,queryNo", oldQuery, queryNo);
-
-    //スクロール
-    gotoHighlight(queryNo);
-
-
-  });
 
   function gotoHighlight(queryNo) {
-
-    const firstHighlight = document.querySelector(".highlight");
-    if (firstHighlight) {
-      firstHighlight.scrollIntoView({ behavior: "instant", block: "center" });
+    const highlightsAll = document.querySelectorAll(".highlight");
+    if (highlightsAll.length > 0) {
+      if (highlightsAll.length > queryNo) {
+        highlightsAll[queryNo].scrollIntoView({ behavior: "instant", block: "center" });
+      }
     }
-
-
-   const highlightsAll = document.querySelectorAll(".highlight");
-   console.log("highlightsAll",highlightsAll[0]);
-
-
-
-
   }
-
-
-
 
 
   // 解説へ
@@ -110,190 +170,152 @@
 
 
   // 各文字の説明へ
-  document.querySelectorAll('.kanji').forEach(tr => {
-    tr.addEventListener('click', () => {
-      //ユーザーが選んだ漢字（親字）はtKanji
-      const tKanji = Array.from(tr.querySelectorAll('td')).map(td => td.textContent.trim());
-      const nwin = window.open('kanji.html', '_blank');
+  const tableClick = document.getElementById('maintableid');
+  tableClick.addEventListener('click', (event) => {
+    // クリックされた要素が .kanji クラスを持つか確認
+    const clickedElement = event.target.closest('.kanji');
+    if (clickedElement) {
+      openKanji(clickedElement.id);
+    }
+  });
 
+  function openKanji(theUnicode) {
+    const nwin = window.open('kanji.html', '_blank');
+    kanjiShugo = JSON.parse(localStorage.getItem('kanjiLocal'));
 
-      // console.log("jsonKanji.length", jsonKanji.length);
-
-
-      //jsonKanjiのなかで、tKanjiと合致するものを選ぶ
-      for (let i = 0; i < jsonKanji.length; i++) {
-        // if(i = jsonKanji.length - 1){alert("漢字が見つかりません");}
-        if (jsonKanji[i].unicode === tKanji[1]) {
-          target = i;
-          continue;
-        }
+    //kanjiShugoのなかで、tKanjiと合致するものを選ぶ
+    for (let i = 0; i < kanjiShugo.length; i++) {
+      if (kanjiShugo[i].unicode === theUnicode) {
+        targetKanji = i;
+        sessionStorage.setItem('checkKanji', targetKanji); //セッション内にtargetKanjiを保存
+        continue;
       }
-      //ユーザーが選んだ漢字（親字）は、jsonKanji内のtheKanji
-      let theKanji = jsonKanji[target];
+    }
 
+    let theKanji = kanjiShugo[targetKanji];
+    nwin.onload = () => {
+      //親字の情報を入れる
+      let title = nwin.document.querySelector('#title_');
+      title.textContent = theKanji.midashi;
 
-      nwin.onload = () => {
+      let jishu = nwin.document.querySelector('#jishu_');
+      jishu.textContent = theKanji.jishu;
 
-        //親字の情報を入れる
-        let title = nwin.document.querySelector('#title_');
-        title.textContent = theKanji.midashi;
+      let midashi = nwin.document.querySelector('#midashi_');
+      midashi.textContent = theKanji.midashi;
 
-        let jishu = nwin.document.querySelector('#jishu_');
-        jishu.textContent = theKanji.jishu;
+      let jikei = nwin.document.querySelector('#jikei_');
+      // let jikeiSrc = jikei.getAttribute('src');
+      jikei.setAttribute('src', theKanji.jikei);
 
-        let midashi = nwin.document.querySelector('#midashi_');
-        midashi.textContent = theKanji.midashi;
+      let mojigun = nwin.document.querySelector('#mojigun_');
+      mojigun.textContent = theKanji.mojigun;
 
-        let jikei = nwin.document.querySelector('#jikei_');
-        // let jikeiSrc = jikei.getAttribute('src');
-        jikei.setAttribute('src', theKanji.jikei);
+      let jislevel = nwin.document.querySelector('#jislevel_');
+      jislevel.textContent = theKanji.jislevel;
 
+      let unicode = nwin.document.querySelector('#unicode_');
+      unicode.textContent = theKanji.unicode;
 
-        let mojigun = nwin.document.querySelector('#mojigun_');
-        mojigun.textContent = theKanji.mojigun;
+      let menkuten = nwin.document.querySelector('#menkuten_');
+      menkuten.textContent = theKanji.menkuten;
 
-        let jislevel = nwin.document.querySelector('#jislevel_');
-        jislevel.textContent = theKanji.jislevel;
+      let jiscode = nwin.document.querySelector('#jiscode_');
+      jiscode.textContent = theKanji.jiscode;
 
-        let unicode = nwin.document.querySelector('#unicode_');
-        unicode.textContent = theKanji.unicode;
+      let chuui = nwin.document.querySelector('#chuui_');
+      chuui.textContent = theKanji.chuui;
 
-        let menkuten = nwin.document.querySelector('#menkuten_');
-        menkuten.textContent = theKanji.menkuten;
+      let yomi = nwin.document.querySelector('#yomi_');
+      yomi.textContent = theKanji.yomi;
 
-        let jiscode = nwin.document.querySelector('#jiscode_');
-        jiscode.textContent = theKanji.jiscode;
+      let raireki = nwin.document.querySelector('#raireki_');
+      raireki.innerHTML = theKanji.raireki;
 
-        let chuui = nwin.document.querySelector('#chuui_');
-        chuui.textContent = theKanji.chuui;
+      let tsukaiwake = nwin.document.querySelector('#tsukaiwake_');
+      tsukaiwake.innerHTML = theKanji.tsukaiwake;
 
-        let yomi = nwin.document.querySelector('#yomi_');
-        yomi.textContent = theKanji.yomi;
+      let sanko = nwin.document.querySelector('#sanko_');
+      sanko.textContent = theKanji.sanko;
 
-        let raireki = nwin.document.querySelector('#raireki_');
-        raireki.innerHTML = theKanji.raireki;
+      let tsukaiwakeEdit = nwin.document.querySelector('#tsukaiwakeEdit_');
+      tsukaiwakeEdit.innerHTML = theKanji.tsukaiwake;
 
-        let tsukaiwake = nwin.document.querySelector('#tsukaiwake_');
-        tsukaiwake.innerHTML = theKanji.tsukaiwake;
+      let theTargetKanji = nwin.document.querySelector('#theTargetKanji');
+      theTargetKanji.textContent = targetKanji;
 
-        let sanko = nwin.document.querySelector('#sanko_');
-        sanko.textContent = theKanji.sanko;
-
-        // };
-
-
-        //異体字の情報を得る
-        for (let i = 1; i < theKanji.nOfJitai; i++) {
-          kariItaiji[i + 1] = theKanji.kanjiCode + "_" + (i + 1);
-
-          for (let j = 0; j < jsonKanji.length; j++) {
-            if (jsonKanji[j].kanjiCode === kariItaiji[i + 1]) {
-              jtarget = j;
-              continue;
-            }
+      //異体字の情報を得る
+      for (let i = 1; i < theKanji.nOfJitai; i++) {
+        kariItaiji[i + 1] = theKanji.kanjiCode + "_" + (i + 1);
+        for (let j = 0; j < kanjiShugo.length; j++) {
+          if (kanjiShugo[j].kanjiCode === kariItaiji[i + 1]) {
+            jtarget = j;
+            continue;
           }
-          //異体字はtheItaiji
-          theItaiji = jsonKanji[jtarget];
-
-          // console.log("異体字が見つかりました！", theItaiji.midashi);
-
-
-          //異体字のデータを追加
-          const kanjigun = nwin.document.getElementById('kanjigun_');
-
-          const newSection = document.createElement('section');
-          newSection.className = "itaiji";
-          newSection.textContent = i + 1;
-
-          const newDiv1 = document.createElement('span');
-          newDiv1.className = "jishu";
-          newDiv1.textContent = theItaiji.jishu;
-
-          const newDiv2 = document.createElement('div');
-          newDiv2.className = "midashi";
-          newDiv2.textContent = theItaiji.midashi;
-
-          const newDiv3 = document.createElement('div');
-          newDiv3.setAttribute('src', theItaiji.jikei);
-          let jikeiContent = `画像データ<br><img  src="${theItaiji.jikei}" width="60">`
-          newDiv3.innerHTML = jikeiContent;
-          newDiv3.className = "jikei";
-
-          const newDiv4 = document.createElement('div');
-          newDiv4.className = "youso";
-
-          const newDiv5 = document.createElement('div');
-          newDiv5.className = "mojigun";
-          newDiv5.textContent = theItaiji.mojigun;
-
-          const newDiv6 = document.createElement('div');
-          newDiv6.className = "jislevel";
-          newDiv6.textContent = theItaiji.jislevel;
-
-          const newDiv7 = document.createElement('div');
-          newDiv7.className = "unicode";
-          newDiv7.textContent = theItaiji.unicode;
-
-          const newDiv8 = document.createElement('div');
-          newDiv8.className = "menkuten";
-          newDiv8.textContent = theItaiji.menkuten;
-
-          const newDiv9 = document.createElement('div');
-          newDiv9.className = "jiscode";
-          newDiv9.textContent = theItaiji.jiscode;
-
-          const newDiv10 = document.createElement('div');
-          newDiv10.className = "chuui";
-          newDiv10.textContent = theItaiji.chuui;
-
-
-          newSection.appendChild(newDiv1);
-          newSection.appendChild(newDiv2);
-          newSection.appendChild(newDiv3);
-
-          newDiv4.appendChild(newDiv5);
-          newDiv4.appendChild(newDiv6);
-          newDiv4.appendChild(newDiv7);
-          newDiv4.appendChild(newDiv8);
-          newDiv4.appendChild(newDiv9);
-          newDiv4.appendChild(newDiv10);
-          newSection.appendChild(newDiv4);
-
-          kanjigun.appendChild(newSection);
-
         }
+        theItaiji = kanjiShugo[jtarget];  //異体字はtheItaiji
 
+        //異体字のデータを追加
+        const kanjigun = nwin.document.getElementById('kanjigun_');
+        const newSection = document.createElement('section');
+        newSection.className = "itaiji";
+        newSection.textContent = i + 1;
 
+        const newDiv1 = document.createElement('span');
+        newDiv1.className = "jishu";
+        newDiv1.textContent = theItaiji.jishu;
 
+        const newDiv2 = document.createElement('div');
+        newDiv2.className = "midashi";
+        newDiv2.textContent = theItaiji.midashi;
 
-      };
+        const newDiv3 = document.createElement('div');
+        newDiv3.setAttribute('src', theItaiji.jikei);
+        let jikeiContent = `画像データ<br><img  src="${theItaiji.jikei}" width="60">`
+        newDiv3.innerHTML = jikeiContent;
+        newDiv3.className = "jikei";
 
+        const newDiv4 = document.createElement('div');
+        newDiv4.className = "youso";
 
+        const newDiv5 = document.createElement('div');
+        newDiv5.className = "mojigun";
+        newDiv5.textContent = theItaiji.mojigun;
 
+        const newDiv6 = document.createElement('div');
+        newDiv6.className = "jislevel";
+        newDiv6.textContent = theItaiji.jislevel;
 
+        const newDiv7 = document.createElement('div');
+        newDiv7.className = "unicode";
+        newDiv7.textContent = theItaiji.unicode;
 
+        const newDiv8 = document.createElement('div');
+        newDiv8.className = "menkuten";
+        newDiv8.textContent = theItaiji.menkuten;
 
+        const newDiv9 = document.createElement('div');
+        newDiv9.className = "jiscode";
+        newDiv9.textContent = theItaiji.jiscode;
 
+        const newDiv10 = document.createElement('div');
+        newDiv10.className = "chuui";
+        newDiv10.textContent = theItaiji.chuui;
 
-      // const parser = new DOMParser();
-      // const doc = parser.parseFromString(htmlContent, 'text/html');
+        newSection.appendChild(newDiv1);
+        newSection.appendChild(newDiv2);
+        newSection.appendChild(newDiv3);
 
+        newDiv4.appendChild(newDiv5);
+        newDiv4.appendChild(newDiv6);
+        newDiv4.appendChild(newDiv7);
+        newDiv4.appendChild(newDiv8);
+        newDiv4.appendChild(newDiv9);
+        newDiv4.appendChild(newDiv10);
+        newSection.appendChild(newDiv4);
 
-
-      // nwin.document.replaceChild(
-      //   nwin.document.adoptNode(doc.documentElement),
-      //   nwin.document.documentElement
-      // );
-
-    })
-  })
-
-
-  //戻る
-  // document.getElementById('headpart').addEventListener('click', () => {
-  //   window.location.href = 'index.html';
-  // });
-
-
-
+        kanjigun.appendChild(newSection);
+      }
+    };
+  }
 }
